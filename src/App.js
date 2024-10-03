1bdef9ca7facff9ecaba48ebf0df8263da0ed75a
abhishek <shriabhishek929@gmail.com>
2024-10-03 10:33:27 +0530

    did somechanges in app

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 // src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbars';
 import Home from './pages/Home';
 import Admin from './pages/Admin';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Quiz from './pages/Quiz';
+import Navbars from './components/Navbars';
 
 function App() {
   return (
     <Router>
-      <Navbar />
+      <Navbars />
       <div className="container mx-auto">
         <Routes>
           <Route path="/" element={<Home />} />
